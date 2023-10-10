acf783f0622e11d38f840497becf821997cf2ed7
Variant Name <your@xmpl.com>
2023-10-10 12:27:30 +0300

    Update

---
+++ app/Http/Controllers/DeliveryController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Delivery;
+use App\Services\Delivery\DeliveryService;
+use Illuminate\Http\Request;
+
+class DeliveryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $deliveries = Delivery::all();
+        return $deliveries;
+    }
+
+    public function calculate(Request $request): array
+    {
+        $data = $request->all();
+
+        $deliverService = new DeliveryService();
+        $deliverService->setData($data);
+
+        return $deliverService->calculate();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Delivery $delivery)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Delivery $delivery)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Delivery $delivery)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Delivery $delivery)
+    {
+        //
+    }
+}
